c4994a770e29ac878908df26fb55d0104248d1bd
Amy Cen <cenamy@gmail.com>
2019-06-24 16:20:37 -0400

    done

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let hour = parseInt(time.split(':')[0])
+  switch (hour) {
+    case hour < 12:
+      return "Good Morning";
+      break;
+    case hour >= 12 && hour <= 17:
+      return "Good Afternoon";
+      break;
+    default:
+      return "Good Evening";
+      break;
+  }
+}
+
+function displayMessage(string) {
+  greeting = document.getElementsById("greeting");
+  greeting.innerHTML = string;
+}
